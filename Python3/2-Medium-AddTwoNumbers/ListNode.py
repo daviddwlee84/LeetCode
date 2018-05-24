96924ae69c8c5d41e439c31d057cf50a1f221526
David Lee <daviddwlee84@gmail.com>
2018-05-24 14:32:15 +0800

    2. Add Two Numbers - Naive

---
+++ Python3/2-Medium-AddTwoNumbers/ListNode.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def listNodeConverter(lst):
+    head = ListNode(0)
+    ptr = head
+    for i, num in enumerate(lst):
+        ptr.val = num
+        if(i < len(lst) - 1):
+            ptr.next = ListNode(0)
+            ptr = ptr.next
+    return head
+
+def printListNode(ln):
+    while(ln != None):
+        print(ln.val, end=" -> ")
+        ln = ln.next
+    print("")
+
+def compareListNode(ln1, ln2):
+    while ln1 != None and ln2 != None:
+        if ln1.val != ln2.val:
+            return False
+        else:
+            ln1 = ln1.next
+            ln2 = ln2.next
+    if ln1 == None and ln2 == None:
+        return True
+    else:
+        return False
---
+++ Python3/2-Medium-AddTwoNumbers/Naive002.py
@@ -0,0 +1,60 @@
+import ListNode as ln
+from ListNode import ListNode
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+
+        head = ListNode(0)
+        l3 = head
+        carry = 0
+        while(True):
+            temp = l1.val + l2.val + carry
+
+            ## other solution:
+            # carry = temp/10
+            # l3.val = temp%10
+
+            if temp > 9:
+                l3.val = temp - 10
+                carry = 1
+            else:
+                l3.val = temp
+                carry = 0
+
+            if(carry == 0 and l1.next == None and l2.next == None):
+                break
+            else:
+                l3.next = ListNode(0)
+                l3 = l3.next
+
+            if(l1.next == None):
+                l1.val = 0
+            else:
+                l1 = l1.next
+            if(l2.next == None):
+                l2.val = 0
+            else:
+                l2 = l2.next
+
+        return head
+
+def main():
+    l1 = ln.listNodeConverter([2, 4, 3])
+    l2 = ln.listNodeConverter([5, 6, 4])
+    ln.printListNode(l1)
+    ln.printListNode(l2)
+    ln.printListNode(Solution().addTwoNumbers(l1, l2))
+
+    l1 = ln.listNodeConverter([1, 8])
+    l2 = ln.listNodeConverter([0])
+    ln.printListNode(l1)
+    ln.printListNode(l2)
+    ln.printListNode(Solution().addTwoNumbers(l1, l2))
+
+if __name__ == "__main__":
+    main()
