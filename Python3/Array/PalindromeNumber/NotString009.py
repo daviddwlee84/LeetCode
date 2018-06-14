50220de545158abb7a0910c7d33013c525666508
David Lee <daviddwlee84@gmail.com>
2018-06-14 09:36:25 +0800

    Add some comment

--- Python3/Array/PalindromeNumber/NotString009.py
+++ Python3/Array/PalindromeNumber/NotString009.py
@@ -5,16 +5,23 @@ class Solution:
         :rtype: bool
         """
         if x < 0:
+            # Negative number isn't palindrome
             return False
         elif x == 0:
+            # Zero is palindrome
             return True
         else:
-            A = 0
-            B = x
-            while B != 0:
-                A = A*10 + B%10
-                B //= 10
-            if x == A:
+            # Reverse x
+            reverse_x = 0
+            temp = x
+            while temp != 0:
+                # reverse_x shift left 1 space and add LSB of temp
+                # (So the LSB of temp will be the MSB of reverse_x and so on.)
+                reverse_x = reverse_x*10 + temp%10
+                temp //= 10
+            
+            # Compare x with reverse x
+            if x == reverse_x:
                 return True
             else:
                 return False
