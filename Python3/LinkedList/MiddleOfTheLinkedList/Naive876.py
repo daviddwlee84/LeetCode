eaf069d9c9a472af5d7dc417407eea489256a905
David Lee <daviddwlee84@gmail.com>
2020-04-08 17:32:41 +0800

    30-Day Challenge Day 8 - Middle of the Linked List

---
+++ Python3/LinkedList/MiddleOfTheLinkedList/Naive876.py
@@ -0,0 +1,15 @@
+from ..ListNodeModule import ListNode
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        A = [head]
+        while A[-1].next:
+            A.append(A[-1].next)
+        
+        return A[len(A) // 2]
