552212cce1d5516808078ace43b8db054ae34245
David Lee <daviddwlee84@gmail.com>
2020-04-11 10:25:35 +0800

    30-Day Challenge Day 10 - Min Stack

--- Python3/Design/MinStack/test_155.py
+++ Python3/Design/MinStack/test_155.py
@@ -1,8 +1,9 @@
 from Naive155 import MinStack as naive
 from Improve155 import MinStack as improve
 
-def test_naive():
-    minStack = naive()
+
+def case1(constructor):
+    minStack = constructor()
     minStack.push(-2)
     minStack.push(0)
     minStack.push(-3)
@@ -11,12 +12,27 @@ def test_naive():
     assert minStack.top() == 0
     assert minStack.getMin() == -2
 
+def case2(constructor):
+    operation = ["MinStack","push","push","push","top","pop","getMin","pop","getMin","pop","push","top","getMin","push","top","getMin","pop","getMin"]
+    value = [[],[2147483646],[2147483646],[2147483647],[],[],[],[],[],[],[2147483647],[],[],[-2147483648],[],[],[],[]]
+    answer = [None,None,None,None,2147483647,None,2147483646,None,2147483646,None,None,2147483647,2147483647,None,-2147483648,-2147483648,None,2147483647]
+    for op, v, ans in zip(operation, value, answer):
+        if op == 'MinStack':
+            minStack = constructor()
+        elif op == 'push':
+            assert minStack.push(v[0]) == ans
+        elif op == 'top':
+            assert minStack.top() == ans
+        elif op == 'getMin':
+            assert minStack.getMin() == ans
+        elif op == 'pop':
+            assert minStack.pop() == ans
+    
+
+def test_naive():
+    case1(naive)
+    case2(naive)
+
 def test_improve():
-    minStack = improve()
-    minStack.push(-2)
-    minStack.push(0)
-    minStack.push(-3)
-    assert minStack.getMin() == -3
-    minStack.pop()
-    assert minStack.top() == 0
-    assert minStack.getMin() == -2
+    case1(improve)
+    case2(improve)
