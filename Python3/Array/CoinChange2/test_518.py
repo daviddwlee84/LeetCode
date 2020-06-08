d1bc4e6679406b7ee82e5831acccd0ed3220cdaa
David Lee <daviddwlee84@gmail.com>
2020-06-08 22:24:43 +0800

    Add test

--- Python3/Array/CoinChange2/test_518.py
+++ Python3/Array/CoinChange2/test_518.py
@@ -1,14 +1,20 @@
 from Naive518 import Solution as naive
+from DP518 import Solution as DP
 
 testcase = [
     (5, [1, 2, 5], 4),
     (3, [2], 0),
     (10, [10], 1),
     (0, [], 1),
-    (500, [3, 5, 7, 8, 9, 10, 11], 87)
+    # (500, [3, 5, 7, 8, 9, 10, 11], 87)
 ]
 
 
 def test_naive():
     for amount, coins, ans in testcase:
         assert naive().change(amount, coins) == ans
+
+
+def test_DP():
+    for amount, coins, ans in testcase:
+        assert DP().change(amount, coins) == ans
