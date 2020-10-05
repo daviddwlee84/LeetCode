0db3c6f6f33d61fe3962dae30830dca4367799fc
David Lee <daviddwlee84@gmail.com>
2020-10-05 14:34:17 +0800

    October 30-Day Challenge Day 3 - K-diff Pairs in an Array

---
+++ Python3/Array/K-diffPairsInAnArray/HT532.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        counter = Counter(nums)
+        if k == 0:
+            return sum(count >= 2 for count in counter.values())
+        else:
+            return sum(num + k in counter for num in counter.keys())
+
+
+# Runtime: 76 ms, faster than 98.74% of Python3 online submissions for K-diff Pairs in an Array.
+# Memory Usage: 15.5 MB, less than 37.53% of Python3 online submissions for K-diff Pairs in an Array.        
+
+# similar with https://leetcode.com/problems/k-diff-pairs-in-an-array/discuss/100135/JavaPython-Easy-Understood-Solution
