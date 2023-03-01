2c22be517e807f72f627a59e1bc25015193d2544
Da-Wei Lee <daweilee@microsoft.com>
2023-03-01 15:33:52 +0800

    912. Sort an Array - Add some TODOs

---
+++ Python3/Array/SortAnArray/HeapSort2_912.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        """
+        https://www.educative.io/answers/heap-implementation-in-python
+        https://www.geeksforgeeks.org/min-heap-in-python/
+        https://medium.com/@hs_pedro/implementing-a-heap-in-python-1036e759e0eb
+        """
+        pass
---
+++ Python3/Array/SortAnArray/ThreeWayQuickSort912.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        """
+        https://www.geeksforgeeks.org/3-way-quicksort-dutch-national-flag/
+        """
+        pass
